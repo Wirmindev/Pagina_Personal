7a90fcc5cad694a11554674d9beb8fce45e9f17c
Daniel López <dalocervera@gmail.com>
2023-07-24 08:12:46 +0200

    .

--- src/component/atom/Video/Video.component.tsx
+++ src/component/atom/Video/Video.component.tsx
@@ -1,10 +1,12 @@
 
- import videoFile from '../../../assets/vid/BG_Page.webm';
+//  import videoFile from '../../../assets/vid/BG_Page.webm';
+import gifFile from '../../../assets/vid/BG_Page.gif'
 
  export const BgVideo: React.FC = () => {
     return( 
-        <div className='absolute top-0 bottom-0 h-full w-full -z-10 '>
-            <video className=' object-center object-cover h-full' autoPlay loop muted src={videoFile}/>
+        <div className='absolute top-0 bottom-0 left-0 right-0 h-full w-full -z-10'>
+            {/* <video className='object-center object-cover h-full' autoPlay loop muted src={videoFile}/> */}
+            <img src={gifFile} className='object-center object-cover h-full w-full'/>
         </div>
     )
 }
